1fabdad4cf0eea45174291f10cb1b7b76f711c7e
Ruben Cano <18100439@ue.edu.pe>
2024-10-12 20:01:08 -0500

    Agregar archivos de proyecto.

---
+++ PARCIAL18100439/Infrastructure/Data/Mechanic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PARCIAL18100439.Infrastructure.Data;
+
+public partial class Mechanic
+{
+    public int Id { get; set; }
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public int? PhoneNumber { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? HireDate { get; set; }
+}
---
+++ PARCIAL18100439/Infrastructure/Data/Parcial202418100439DbContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PARCIAL18100439.Infrastructure.Data;
+
+public partial class Parcial202418100439DbContext : DbContext
+{
+    public Parcial202418100439DbContext()
+    {
+    }
+
+    public Parcial202418100439DbContext(DbContextOptions<Parcial202418100439DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Mechanic> Mechanic { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=RUBEN\\SQLEXPRESS;Database=Parcial202418100439DB;Integrated Security=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Mechanic>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Mechanic__3213E83F5A92B930");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(1)
+                .IsUnicode(false);
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(1)
+                .IsUnicode(false);
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(1)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
